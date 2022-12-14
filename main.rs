afca6e9bb426503529e49d3240a3e2dc59c0109d
Ashley Katt <46717680+ashley-katt@users.noreply.github.com>
2022-12-14 12:38:38 -0600

    Complete!

--- main.rs
+++ main.rs
@@ -1,11 +1,14 @@
+use colored::ColoredString;
 use rand; // 0.8.5
 use rand::Rng;
 use std::fmt;
 use std::io;
+use colored::Colorize;
 
 struct Board<const WIDTH: usize, const HEIGHT: usize> {
     board_data: [[Tile; HEIGHT]; WIDTH],
-    game_over: bool
+    game_over: bool,
+    first_reveal: bool
 }
 impl<const WIDTH: usize, const HEIGHT: usize> Board<WIDTH, HEIGHT> {
     fn new(mine_count: usize) -> Board<WIDTH, HEIGHT> {
@@ -16,7 +19,7 @@ impl<const WIDTH: usize, const HEIGHT: usize> Board<WIDTH, HEIGHT> {
             let y = rng.gen_range(0..HEIGHT);
             board_data[x][y] = Tile::Mine;
         }
-        Board {board_data, game_over: false}
+        Board {board_data, game_over: false, first_reveal: true}
     }
     fn is_solved(&self) -> bool {
         for y in 0..HEIGHT {
@@ -29,16 +32,33 @@ impl<const WIDTH: usize, const HEIGHT: usize> Board<WIDTH, HEIGHT> {
         return true;
     }
     fn reveal(&mut self, x: usize, y: usize) -> bool {
+        if self.first_reveal {
+            self.board_data[x][y] = Tile::Safe;
+            for (xs, ys) in self.get_adjacent(x, y) {
+                self.board_data[xs][ys] = Tile::Safe;
+            }
+            self.first_reveal = false;
+        }
         match self.board_data[x][y] {
             Tile::Mine => true,
             Tile::Safe => {
-                let mut s = 0;
-                for (xs, ys) in self.get_adjacent(x, y) {
-                    if let Tile::Mine | Tile::FlaggedMine = self.board_data[xs][ys] {
-                        s += 1;
+                let mut to_reveal = vec![(x, y)];
+                while let Some((x, y)) = to_reveal.pop() {
+                    let mut s = 0;
+                    for (xs, ys) in self.get_adjacent(x, y) {
+                        if let Tile::Mine | Tile::FlaggedMine = self.board_data[xs][ys] {
+                            s += 1;
+                        }
+                    }
+                    self.board_data[x][y] = Tile::Revealed(s);
+                    if s == 0 {
+                        for l @ (xs, ys) in self.get_adjacent(x, y) {
+                            if let Tile::Mine | Tile::Safe = self.board_data[xs][ys] {
+                                to_reveal.push(l);
+                            }
+                        }
                     }
                 }
-                self.board_data[x][y] = Tile::Revealed(s);
                 false
             }
             _ => false
@@ -71,19 +91,19 @@ impl<const W: usize, const H: usize> fmt::Display for Board<W, H> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "     ")?;
         for x in 0..W {
-            write!(f, "{:<2} ", x+1)?;
+            write!(f, "{:<2} ", (x+1).to_string().bright_black())?;
         }
         writeln!(f, "")?;
-        write!(f, "   +")?;
+        write!(f, "   {}", "+".bright_black())?;
         for _ in 0..W {
-            write!(f, "---")?;
+            write!(f, "{}", "---".bright_black())?;
         }
         writeln!(f, "")?;
         for y in 0..H {
-            write!(f, "{:>2} | ", y+1)?;
+            write!(f, "{:>2} {} ", (y+1).to_string().bright_black(), "|".bright_black())?;
             for x in 0..W {
                 let tile = self.board_data[x][y];
-                write!(f, "{}  ", self.board_data[x][y].get_char(self.game_over))?;
+                write!(f, "{}  ", tile.get_char(self.game_over))?;
             }
             writeln!(f, "")?;
         }
@@ -100,30 +120,51 @@ enum Tile {
     Revealed(u8)
 }
 impl Tile {
-    fn get_char(&self, game_over: bool) -> char {
+    fn get_char(&self, game_over: bool) -> ColoredString {
         if game_over {
             match self {
-                Self::Mine => 'X',
-                Self::Safe => '-',
-                Self::FlaggedSafe => 'F',
-                Self::FlaggedMine => '!',
-                Self::Revealed(n) => n.to_string().chars().next().unwrap()
+                Self::Mine => "X".red(),
+                Self::Safe => "-".bright_white(),
+                Self::FlaggedSafe => "F".yellow(),
+                Self::FlaggedMine => "!".yellow(),
+                Self::Revealed(1) => "1".blue(),
+                Self::Revealed(2) => "2".green(),
+                Self::Revealed(3) => "3".bright_red(),
+                Self::Revealed(4) => "4".purple(),
+                Self::Revealed(5) => "5".red(),
+                Self::Revealed(6) => "6".cyan(),
+                Self::Revealed(7) => "7".white(),
+                Self::Revealed(8) => "8".bright_black(),
+                _ => " ".white(),
             }
         } else {
             match self {
-                Self::Mine => '-',
-                Self::Safe => '-',
-                Self::FlaggedSafe => 'F',
-                Self::FlaggedMine => 'F',
-                Self::Revealed(n) => n.to_string().chars().next().unwrap()
+                Self::Mine => "-".white(),
+                Self::Safe => "-".white(),
+                Self::FlaggedSafe => "F".yellow(),
+                Self::FlaggedMine => "F".yellow(),
+                Self::Revealed(1) => "1".blue(),
+                Self::Revealed(2) => "2".green(),
+                Self::Revealed(3) => "3".bright_red(),
+                Self::Revealed(4) => "4".purple(),
+                Self::Revealed(5) => "5".red(),
+                Self::Revealed(6) => "6".cyan(),
+                Self::Revealed(7) => "7".white(),
+                Self::Revealed(8) => "8".bright_black(),
+                _ => " ".white(),
             }
         }
     }
 }
-
+enum EndState {
+    WIN, LOSS, QUIT
+}
 fn main() {
-    let mut board: Board<15,13> = Board::new(40);
-    while !board.is_solved() {
+    const WIDTH: usize = 8;
+    const HEIGHT: usize = 8;
+    let mut board: Board<WIDTH,HEIGHT> = Board::new(10);
+    let state = loop {
+        clear_screen();
         println!("{}", board);
         println!();
         println!("Enter Command: ");
@@ -134,6 +175,7 @@ fn main() {
             eprintln!("Failed to read input!");
             return;
         }
+        input.push(' ');
         let mut sp = input.split(" ");
         match sp.next() {
             Some("flag") | Some("f") => {
@@ -141,8 +183,11 @@ fn main() {
                     if let Some(ys) = sp.next() {
                         if let Ok(x) = xs.trim().parse::<usize>() {
                             if let Ok(y) = ys.trim().parse::<usize>() {
+                                if x > WIDTH || y > HEIGHT {
+                                    println!("Location not on board.");
+                                    continue;
+                                }
                                 board.flag(x-1usize, y-1usize);
-                                continue;
                             }
                         }
                     }
@@ -154,13 +199,22 @@ fn main() {
                     if let Some(ys) = sp.next() {
                         if let Ok(x) = xs.trim().parse::<usize>() {
                             if let Ok(y) = ys.trim().parse::<usize>() {
-                                board.reveal(x-1usize, y-1usize);
-                                continue;
+                                if x > WIDTH || y > HEIGHT {
+                                    println!("Location not on board.");
+                                    continue;
+                                }
+                                let r = board.reveal(x-1usize, y-1usize);
+                                if r {
+                                    break EndState::LOSS;
+                                }
                             }
                         }
                     }
                 }
                 println!("Malformed command.");
+            }
+            Some("quit") => {
+                break EndState::QUIT;
             }
             Some(_) => {
                 println!("Unrecognized command.");
@@ -169,5 +223,30 @@ fn main() {
                 println!("Please input a command.");
             }
         }
+        if board.is_solved() {
+            break EndState::WIN;
+        }
+    };
+    board.game_over = true;
+    match state {
+        EndState::WIN => {
+            clear_screen();
+            println!("{}", board);
+            println!("YOU WIN!");
+        },
+        EndState::LOSS => {
+            clear_screen();
+            println!("{}", board);
+            println!("YOU LOSE");
+        },
+        EndState::QUIT => {
+            clear_screen();
+            println!("Quitting!");
+        },
     }
+
+}
+
+fn clear_screen() {
+    print!("\x1B[2J\x1B[1;1H");
 }
